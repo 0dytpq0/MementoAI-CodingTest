5928b7131a1f25d27ca1058e4e1d086415898c03
Joseph Park <rkfnahs12e@gmail.com>
2024-09-30 14:24:02 +0900

    feat : 코딩 테스트 문제 풀이 완료

---
+++ index.js
@@ -0,0 +1,54 @@
+const readline = require("readline");
+const { stdin: input, stdout: output } = require("process");
+
+// 입출력 예 arr = [4,3,2,1], [10] return = [4,3,2], [-1]
+
+// 가장 작은 수를 제거하는 함수
+const removeMinNumber = (arr) => {
+  // 전달된 arr의 값들 중 가장 작은 값을 제거
+  const removedArr = [...arr];
+
+  // 1번 풀이 : 배열을 3번 순회하므로 시간 복잡도 = O(n) { O(n) * 3 ) }
+  // removedArr.splice(arr.indexOf(Math.min(...arr)), 1);
+
+  // 2번 풀이 : 배열 순회를 줄인 풀이로 시간 복잡도 = O(n log n) { O(n log n) + O(1) }
+  removedArr.sort((a, b) => b - a);
+  removedArr.pop();
+
+  const result = checkArrLength(removedArr);
+
+  return result;
+};
+
+// arr의 길이에 따라 결과값을 리턴해주는 함수
+const checkArrLength = (arr) => {
+  // arr의 길이가 0 이하라면 1 아니라면 arr 리턴
+  const result = arr.length <= 0 ? [-1] : arr;
+
+  return result;
+};
+
+// 1. 콘솔창으로 값을 입력받는 경우.
+const rl = readline.createInterface({ input, output });
+rl.question(
+  "작은 수를 제거할 값들을 입력해주세요. ex) 4,3,2,1 : ",
+  (numbers) => {
+    // 입력 받은 값을 Number타입의 값이 담긴 배열로
+    const arr = numbers.split(",").map((num) => Number(num));
+    const result = removeMinNumber(arr);
+    console.log(result);
+    rl.close();
+  }
+);
+
+// 2. 입력값이 미리 제공되는 경우
+function main(arr) {
+  const result = removeMinNumber(arr);
+
+  return result;
+}
+
+const testArr1 = [4, 3, 2, 1];
+const testArr2 = [10];
+
+const result = main(testArr1);
